9a55eebd2e026063f566101a4e5314442f5b2fb4
eSaniello <shaniel29samadhan@gmail.com>
2019-10-28 12:21:25 -0300

    working on database

--- app.py
+++ app.py
@@ -1,10 +1,4 @@
-from flask import Flask, render_template
+from app import app
 
-app = Flask(__name__)
-
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == '__main__':
+    app.run()
